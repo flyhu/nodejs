0f9ccc466d70699e9ac7f9bd292e719acd781f0f
flyhu <252174170@qq.com>
2017-06-20 13:38:32 +0800

    Update pk10crawler.js

--- pk10crawler.js
+++ pk10crawler.js
@@ -3,7 +3,7 @@ var request = require('request');
 var async = require('async');
 var cheerio = require('cheerio');
 
-var days = 2
+var days = 10
 var results = {}
 var datelist=[]
 
@@ -14,12 +14,13 @@ function getdate(days){
 		var day=d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
 		datelist.push(day)
 	}
-	console.log(datelist)
+	datelist.reverse()
 }
 
-function crawl(day,error, response, body){
-		
-async.eachSeries(days, function(day, callback) {
+function crawl(datelist){
+
+//async.eachSeries异步操作		
+async.eachSeries(datelist, function(day, callback) {
 	var options = {
 	    url: 'http://www.lecai.com/lottery/draw/list/557?d='+day,
 	    headers: {
@@ -33,8 +34,9 @@ async.eachSeries(days, function(day, callback) {
 	    if (!error && response.statusCode == 200) {
 	      $ = cheerio.load(body);
 	      var result = [];
-	      var trs = $("#draw_list tbody tr");
 
+//页面信息抓取
+	      var trs = $("#draw_list tbody tr");
 	      trs.each(function(){                    
 	                    var tr = $(this);
 	                    var balls = tr.find(".ball_1");
@@ -49,15 +51,17 @@ async.eachSeries(days, function(day, callback) {
 	                    result.push(list);                        
 	        })
 
-	     results[day] = result; 
-fs.writeFile('./data/' + day + '.json', JSON.stringify(results)
-	,function (err) {
-        if (err) throw err;
-        console.log('写入完成');
-})
-	    }
-	  callback(error, results);  
-	})
+	     results[day] = result 
+
+//保存文件
+		fs.writeFile('./data/pk10.json', JSON.stringify(results)
+			,function (err) {
+		        if (err) throw err;
+		        console.log('写入完成');
+		})
+			    }
+			  callback(error, results);  
+			})
 
 
 	
@@ -67,5 +71,5 @@ if (err) return console.error(err.stack)})
 }
 
 getdate(days)
-
+crawl(datelist)
 		
